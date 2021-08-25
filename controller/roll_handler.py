c633248672d6eb14ad2f591338c2b094a1d6bb5c
= <=>
2021-08-25 14:31:06 +0100

    Roll handler implemented and unit tested.

--- controller/roll_handler.py
+++ controller/roll_handler.py
@@ -2,7 +2,9 @@ from enum import Enum
 import random
 import re
 
-ROLL_FULL_MATCHER = re.compile("([+-]*\d+(:?d\d+)?)+")
+
+ONE_DIE_ROLL_MATCHER = re.compile("((\\d*)d(\\d+))")
+ONE_D_TWENTY_MATCHER = re.compile("([+-]*\\d*)*1d20([+-]*\\d+)*")
 
 
 class RollType(Enum):
@@ -12,17 +14,80 @@ class RollType(Enum):
     ADVANTAGE = 1
     SUPER_ADVANTAGE = 2
 
-# ([+-]*\d+(:?d\d+)?)
+    def get_postfix(self):
+        if self is RollType.SUPER_DISADVANTAGE:
+            return "with super-disadvantage"
+        elif self is RollType.DISADVANTAGE:
+            return "with disadvantage"
+        elif self is RollType.STANDARD:
+            return ""
+        elif self is RollType.ADVANTAGE:
+            return "with advantage"
+        elif self is RollType.SUPER_ADVANTAGE:
+            return "with super-advantage"
 
 
-def roll(roll_command, roll_type=RollType.STANDARD):
-    pass
+class RollHandler:
+    def __init__(self):
+        pass
+
+    def roll(self, expression, roll_type=RollType.STANDARD):
+        expression = self._preformat_expression(expression)
+        if self._is_valid_expression(expression, roll_type):
+            roll_match = re.search(ONE_DIE_ROLL_MATCHER, expression)
+            if roll_match:
+                full_match, dice_count, face_count = roll_match.groups()
+                total = 0
+                adv_roll_count = abs(roll_type.value) + 1
+                print(f"Rolling {full_match} {roll_type.get_postfix()}:")
+                for d in range(int(dice_count)):
+                    rolls = [random.randint(1, int(face_count)) for _ in range(adv_roll_count)]
+                    print("\n".join([str(s) for s in rolls]))
+                    total += max(rolls) if roll_type.value >= 0 else min(rolls)
+                expression = expression.replace(full_match, str(total), 1)
+            to_rtn = eval(expression)
+            print(expression, "=", to_rtn)
+            return to_rtn
+
+    def roll_advantage(self, roll_expression):
+        return self.roll(roll_expression, roll_type=RollType.ADVANTAGE)
+
+    def roll_disadvantage(self, roll_expression):
+        return self.roll(roll_expression, roll_type=RollType.DISADVANTAGE)
+
+    def roll_super_advantage(self, roll_expression):
+        return self.roll(roll_expression, roll_type=RollType.SUPER_ADVANTAGE)
+
+    def roll_super_disadvantage(self, roll_expression):
+        return self.roll(roll_expression, roll_type=RollType.SUPER_DISADVANTAGE)
+
+    @staticmethod
+    def _is_valid_expression(expression, roll_type=RollType.STANDARD):
+        preformatted_expression = RollHandler._preformat_expression(expression)
+        expression_without_dice = re.sub(ONE_DIE_ROLL_MATCHER, "0", preformatted_expression)
+        if len(list(re.finditer(ONE_DIE_ROLL_MATCHER, preformatted_expression))) > 1:
+            print("Multiple dice rolls found in expression e.g 1d20 + 1d4")
+            return False
+        elif not re.fullmatch(ONE_D_TWENTY_MATCHER, preformatted_expression) and roll_type is not RollType.STANDARD:
+            print("Non-1d20 expression found for roll which has advantage or disadvantage")
+            return False
+        try:
+            eval(expression_without_dice)
+        except SyntaxError:
+            print("Invalid math syntax in expression.")
+            return False
+        return True
+
+    @staticmethod
+    def _preformat_expression(roll_expression):
+        if roll_expression[0] is "d":
+            roll_expression = "1" + roll_expression
+        return re.sub("\\s", "", roll_expression)
 
 
 if __name__ == '__main__':
-    print(Skills.SLEIGHT_OF_HAND.is_dex())
-    for v in filter(lambda s: s.is_dex(), Skills):
-        print(v)
-    my_list = [0, 1, 2, 3, 4]
-    for val in filter(lambda x: x % 2 == 0, my_list):
-        print(val)
+    in_str = "d12+5"
+    handler = RollHandler()
+    print("Input:", in_str)
+    v = handler.roll(in_str, roll_type=RollType.DISADVANTAGE)
+    # print("Result:", v)
---
+++ test/controller/test_roll_handler.py
@@ -0,0 +1,118 @@
+import unittest
+from controller.roll_handler import RollHandler, RollType
+
+SAMPLE_SIZE = 80
+
+
+class TestRollHandler(unittest.TestCase):
+
+    def setUp(self):
+        self.handler = RollHandler()
+
+    def test_isValidExpress_d20_shouldReturnTrue(self):
+        in_expression = "d20 "
+        result = self.handler._is_valid_expression(in_expression)
+        self.assertTrue(result)
+
+    def test_isValidExpress_d12plus5_shouldReturnTrue(self):
+        in_expression = "d12 + 5 "
+        result = RollHandler._is_valid_expression(in_expression)
+        self.assertTrue(result)
+
+    def test_isValidExpression_1d20_shouldReturnTrue(self):
+        in_expression = "1d20 "
+        result = RollHandler._is_valid_expression(in_expression)
+        self.assertTrue(result)
+
+    def test_isValidExpression_1d20plus5_shouldReturnTrue(self):
+        in_expression = " 1d20+5"
+        result = RollHandler._is_valid_expression(in_expression)
+        self.assertTrue(result)
+
+    def test_isValidExpression_1d20minus7_shouldReturnTrue(self):
+        in_expression = "1d20 - 7"
+        result = RollHandler._is_valid_expression(in_expression)
+        self.assertTrue(result)
+
+    def test_isValidExpression_1d20minus3withAdvantage_shouldReturnTrue(self):
+        in_expression = "1d20- 3"
+        result = RollHandler._is_valid_expression(in_expression, roll_type=RollType.ADVANTAGE)
+        self.assertTrue(result)
+
+    def test_isValidExpression_1d20plus1d4plus5_shouldReturnFalse(self):
+        in_expression = "1d20+1d4+5"
+        result = RollHandler._is_valid_expression(in_expression)
+        self.assertFalse(result)
+
+    def test_isValidExpression_1d12minus7withAdvantage_shouldReturnFalse(self):
+        in_expression = "1d12-7"
+        result = RollHandler._is_valid_expression(in_expression, roll_type=RollType.ADVANTAGE)
+        self.assertFalse(result)
+
+    def test_isValidExpression_2d20minus2withAdvantage_shouldReturnFalse(self):
+        in_expression = "2d20-2"
+        result = RollHandler._is_valid_expression(in_expression, roll_type=RollType.ADVANTAGE)
+        self.assertFalse(result)
+
+    def test_isValidExpression_1d12minus7WithSuperDisadvantage_shouldReturnFalse(self):
+        in_expression = "1d12-7"
+        result = RollHandler._is_valid_expression(in_expression, roll_type=RollType.SUPER_DISADVANTAGE)
+        self.assertFalse(result)
+
+    def test_isValidExpression_invalidMath_shouldReturnFalse(self):
+        in_expression = "1d12-*-7"
+        result = RollHandler._is_valid_expression(in_expression)
+        self.assertFalse(result)
+
+    def test_roll_1d8plus3_shouldHaveSamplesInRange(self):
+        in_expression = "1d8+3"
+        for _ in range(SAMPLE_SIZE):
+            result = self.handler.roll(in_expression)
+            self.assertGreaterEqual(result, 4)
+            self.assertLessEqual(result, 11)
+
+    def test_roll_1d20_shouldHaveSamplesInRange(self):
+        in_expression = "1d20"
+        for _ in range(SAMPLE_SIZE):
+            result = self.handler.roll(in_expression)
+            self.assertGreaterEqual(result, 1)
+            self.assertLessEqual(result, 20)
+
+    def test_roll_1d20withSuperAdvantage_shouldHaveSamplesInRange(self):
+        in_expression = "1d20"
+        for _ in range(SAMPLE_SIZE):
+            result = self.handler.roll(in_expression, RollType.SUPER_ADVANTAGE)
+            self.assertGreaterEqual(result, 1)
+            self.assertLessEqual(result, 20)
+
+    def test_roll_1d20plus2_shouldHaveSamplesInRange(self):
+        in_expression = "1d20+2"
+        for _ in range(SAMPLE_SIZE):
+            result = self.handler.roll(in_expression)
+            self.assertGreaterEqual(result, 3)
+            self.assertLessEqual(result, 22)
+
+    def test_roll_1d20minus5_shouldHaveSamplesInRange(self):
+        in_expression = "1d20-5"
+        for _ in range(SAMPLE_SIZE):
+            result = self.handler.roll(in_expression)
+            self.assertGreaterEqual(result, -4)
+            self.assertLessEqual(result, 15)
+
+    def test_roll_2d12minus1_shouldHaveSamplesInRange(self):
+        in_expression = "2d12-1"
+        for _ in range(SAMPLE_SIZE):
+            result = self.handler.roll(in_expression)
+            self.assertGreaterEqual(result, 1)
+            self.assertLessEqual(result, 23)
+
+    def test_roll_10d6minus5_shouldHaveSamplesInRange(self):
+        in_expression = "10d6+5"
+        for _ in range(SAMPLE_SIZE):
+            result = self.handler.roll(in_expression)
+            self.assertGreaterEqual(result, 11)
+            self.assertLessEqual(result, 65)
+
+
+if __name__ == '__main__':
+    unittest.main()
